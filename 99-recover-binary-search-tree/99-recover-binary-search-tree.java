0be2225682184999c3be547eb3800c7b86d811f0
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-17 14:05:39 +0530

    Time: 3 ms (83.12%), Space: 47 MB (68.91%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -20,6 +20,38 @@ class Solution {
 //         List<TreeNode> inorderList = new ArrayList<>();
 //         inorder(root, inorderList);
         
+//         List<TreeNode> sorted = new ArrayList<>(inorderList);
+//         Collections.sort(sorted, Comparator.comparingInt(node -> node.val));
+        
+//         TreeNode n1 = null;
+//         TreeNode n2 = null;
+//         for(int i=0 ; i<sorted.size() ; i++) {
+//             if(inorderList.get(i).val != sorted.get(i).val) {
+//                 if(n1 == null)
+//                     n1 = inorderList.get(i);
+//                 else
+//                     n2 = inorderList.get(i);
+//             }
+//         }
+//         swap(n1, n2);
+//     }
+    
+//     private void inorder(TreeNode root, List<TreeNode> inorderList) {
+//         if(root==null)
+//             return;
+        
+//         inorder(root.left, inorderList);
+//         inorderList.add(root);
+//         inorder(root.right, inorderList);
+//     }
+    
+    
+    
+    // 2) By storing inorder in a list
+//     public void recoverTree(TreeNode root) {
+//         List<TreeNode> inorderList = new ArrayList<>();
+//         inorder(root, inorderList);
+        
 //         TreeNode l1 = null, r1 = null;
 //         TreeNode r2 = null, l2 = null;
         
@@ -41,12 +73,6 @@ class Solution {
 //             swap(l1, r2);
 //     }
     
-    private void swap(TreeNode n1, TreeNode n2) {
-        int temp = n1.val;
-        n1.val = n2.val;
-        n2.val = temp;
-    }
-    
 //     private void inorder(TreeNode root, List<TreeNode> inorderList) {
 //         if(root==null)
 //             return;
@@ -57,33 +83,46 @@ class Solution {
 //     }
     
     
-    // 1) By storing inorder in a list
+    
+    // 3) Constant space: By doing inorder traversal 
+    private TreeNode prev = null;
+    private TreeNode l1 = null;
+    private TreeNode l2 = null;
+    private TreeNode r1 = null;
+    private TreeNode r2 = null;
+    
     public void recoverTree(TreeNode root) {
-        List<TreeNode> inorderList = new ArrayList<>();
-        inorder(root, inorderList);
+        inorder(root);
         
-        List<TreeNode> sorted = new ArrayList<>(inorderList);
-        Collections.sort(sorted, Comparator.comparingInt(node -> node.val));
-        
-        TreeNode n1 = null;
-        TreeNode n2 = null;
-        for(int i=0 ; i<sorted.size() ; i++) {
-            if(inorderList.get(i).val != sorted.get(i).val) {
-                if(n1 == null)
-                    n1 = inorderList.get(i);
-                else
-                    n2 = inorderList.get(i);
-            }
-        }
-        swap(n1, n2);
+        if(l2 != null)
+            swap(l1, r2);
+        else
+            swap(l1, r1);
     }
     
-    private void inorder(TreeNode root, List<TreeNode> inorderList) {
+    private void inorder(TreeNode root) {
         if(root==null)
             return;
         
-        inorder(root.left, inorderList);
-        inorderList.add(root);
-        inorder(root.right, inorderList);
+        inorder(root.left);
+        
+        if(prev!=null && prev.val>root.val) {
+            if(l1==null) {
+                l1 = prev;
+                r1 = root;
+            } else {
+                l2 = prev;
+                r2 = root;
+            }
+        }
+        prev = root;
+        
+        inorder(root.right);
+    }
+    
+    private void swap(TreeNode n1, TreeNode n2) {
+        int temp = n1.val;
+        n1.val = n2.val;
+        n2.val = temp;
     }
 }
