181b5bb752e1dffc1a7998d17274a9d8dbea3e43
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-18 21:26:35 +0530

    Time: 2 ms (67.79%), Space: 46.4 MB (63.22%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -111,41 +111,41 @@ class Solution {
             - graph should be fully connected. A single component.
             - graph should have exactly n-1 edges
     */
-    public boolean validTree(int n, int[][] edges) {
-        if(edges.length != n-1)
-            return false;
+//     public boolean validTree(int n, int[][] edges) {
+//         if(edges.length != n-1)
+//             return false;
         
-        List<Integer>[] adjList = getAdjList(n, edges);
+//         List<Integer>[] adjList = getAdjList(n, edges);
         
-        Set<Integer> seen = new HashSet<>();
-        Queue<Integer> q = new ArrayDeque<>();
-        seen.add(0);
-        q.add(0);
+//         Set<Integer> seen = new HashSet<>();
+//         Queue<Integer> q = new ArrayDeque<>();
+//         seen.add(0);
+//         q.add(0);
         
-        while(!q.isEmpty()) {
-            int src = q.remove();
+//         while(!q.isEmpty()) {
+//             int src = q.remove();
             
-            for(int nbr : adjList[src]) {
-                if(!seen.contains(nbr)) {
-                    seen.add(nbr);
-                    q.add(nbr);
-                }
-            }
-        }
-        return seen.size()==n;
-    }
+//             for(int nbr : adjList[src]) {
+//                 if(!seen.contains(nbr)) {
+//                     seen.add(nbr);
+//                     q.add(nbr);
+//                 }
+//             }
+//         }
+//         return seen.size()==n;
+//     }
     
-    private List<Integer>[] getAdjList(int n, int[][] edges) {
-        List<Integer>[] adjList = new ArrayList[n];
-        for(int i=0 ; i<n ; i++)
-            adjList[i] = new ArrayList<>();
+//     private List<Integer>[] getAdjList(int n, int[][] edges) {
+//         List<Integer>[] adjList = new ArrayList[n];
+//         for(int i=0 ; i<n ; i++)
+//             adjList[i] = new ArrayList<>();
         
-        for(int[] edge : edges) {            
-            adjList[edge[0]].add(edge[1]);
-            adjList[edge[1]].add(edge[0]);
-        }
-        return adjList;
-    }
+//         for(int[] edge : edges) {            
+//             adjList[edge[0]].add(edge[1]);
+//             adjList[edge[1]].add(edge[0]);
+//         }
+//         return adjList;
+//     }
     
     
     // 5) Advanced Graph Theory + Union Find
@@ -154,7 +154,58 @@ class Solution {
             - graph should be fully connected. A single component.
             - graph should have exactly n-1 edges
     */
-//     public boolean validTree(int n, int[][] edges) {
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length!=n-1)
+            return false;
         
-//     }
+        DisjointSet ds = new DisjointSet(n);
+        for(int[] edge : edges) {
+            ds.union(edge[0], edge[1]);
+        }
+        
+        return ds.components==1;
+    }
+    
+    class DisjointSet {
+        int[] root;
+        int[] rank;
+        int components;
+        
+        DisjointSet(int n) {
+            root = new int[n];
+            rank = new int[n];
+            components = n;
+            
+            for(int i=0 ; i<n ; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            if(x==root[x])
+                return x;
+            
+            root[x] = find(root[x]);
+            return root[x];
+        }
+        
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            
+            if(rootX != rootY) {
+                if(rank[rootX] < rank[rootY])
+                    root[rootX] = rootY;
+                else if(rank[rootX] > rank[rootY])
+                    root[rootY] = rootX;
+                else {
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                
+                components--;
+            }
+        }
+    }
 }
