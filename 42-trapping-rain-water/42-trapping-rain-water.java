cc298395b7a14d7dd5152cde36973aa3aaa0d350
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-18 23:59:44 +0530

    Time: 1 ms (99.76%), Space: 48.5 MB (62.44%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,22 +1,51 @@
 class Solution {
 
+    // 1)
+//     public int trap(int[] height) {
+//         int n = height.length;
+        
+//         int[] suffix = new int[n];
+//         suffix[n-1] = height[n-1];
+//         for(int i=n-2 ; i>=0 ; i--) {
+//             suffix[i] = Math.max(height[i], suffix[i+1]);
+//         }
+        
+//         int prefixMax = height[0];
+//         int trapped = 0;
+//         for(int i=1 ; i<height.length-1 ; i++) {
+//             int diff = Math.min(prefixMax, suffix[i+1]) - height[i];
+//             if(diff > 0)
+//                 trapped += diff;
+            
+//             prefixMax = Math.max(prefixMax, height[i]);
+//         }
+//         return trapped;
+//     }
+    
+    // 2)
     public int trap(int[] height) {
         int n = height.length;
         
-        int[] suffix = new int[n];
-        suffix[n-1] = height[n-1];
-        for(int i=n-2 ; i>=0 ; i--) {
-            suffix[i] = Math.max(height[i], suffix[i+1]);
-        }
+        int left = 0;
+        int right = n-1;
+        int leftMax = 0;
+        int rightMax = 0;
         
-        int prefixMax = height[0];
         int trapped = 0;
-        for(int i=1 ; i<height.length-1 ; i++) {
-            int diff = Math.min(prefixMax, suffix[i+1]) - height[i];
-            if(diff > 0)
-                trapped += diff;
-            
-            prefixMax = Math.max(prefixMax, height[i]);
+        while(left < right) {
+            if(height[left] < height[right]) {
+                if(height[left] > leftMax)
+                    leftMax = height[left];
+                else
+                    trapped += leftMax - height[left];
+                left++;
+            } else {
+                if(height[right] > rightMax)
+                    rightMax = height[right];
+                else
+                    trapped += rightMax - height[right];
+                right--;   
+            }
         }
         return trapped;
     }
