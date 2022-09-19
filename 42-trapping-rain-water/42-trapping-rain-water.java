e3fcd6ff9e7e48769a20af460d87dd5261b97b38
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-19 09:35:59 +0530

    Time: 1 ms (99.76%), Space: 48.7 MB (52.02%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,52 +1,73 @@
 class Solution {
-
-    // 1)
+    
+    // 1) Brute Force
 //     public int trap(int[] height) {
 //         int n = height.length;
         
-//         int[] suffix = new int[n];
-//         suffix[n-1] = height[n-1];
-//         for(int i=n-2 ; i>=0 ; i--) {
-//             suffix[i] = Math.max(height[i], suffix[i+1]);
-//         }
-        
-//         int prefixMax = height[0];
-//         int trapped = 0;
-//         for(int i=1 ; i<height.length-1 ; i++) {
-//             int diff = Math.min(prefixMax, suffix[i+1]) - height[i];
-//             if(diff > 0)
-//                 trapped += diff;
+//         int res = 0;
+//         for(int i=1 ; i<n-1 ; i++) {
+//             int leftMax = 0;
+//             int rightMax = 0;
             
-//             prefixMax = Math.max(prefixMax, height[i]);
+//             for(int l=0 ; l<i ; l++)
+//                 leftMax = Math.max(leftMax, height[l]);
+//             for(int r=i+1 ; r<n ; r++)
+//                 rightMax = Math.max(rightMax, height[r]);
+            
+//             int trapped = Math.min(leftMax, rightMax) - height[i];
+//             if(trapped > 0)
+//                 res += trapped;
 //         }
-//         return trapped;
+//         return res;
 //     }
     
-    // 2)
+    
+    // 2) Using DP -> Prefix max and Suffix max. O(n) space
+//     public int trap(int[] height) {
+//         int n = height.length;
+        
+//         int[] suffixMax = new int[n];
+        
+//         suffixMax[n-1] = height[n-1];
+//         for(int i=n-2 ; i>=0 ; i--)
+//             suffixMax[i] = Math.max(suffixMax[i+1], height[i]);
+        
+//         int leftMax = 0;
+//         int res = 0;
+//         for(int i=0 ; i<n-1 ; i++) {
+//             int trap = Math.min(leftMax, suffixMax[i+1]) - height[i];
+//             if(trap > 0)
+//                 res += trap;
+            
+//             leftMax = Math.max(leftMax, height[i]);
+//         }
+//         return res;
+//     }
+    
+    
+    // 3) Using 2 pointers. O(1) space
     public int trap(int[] height) {
         int n = height.length;
         
-        int left = 0;
-        int right = n-1;
-        int leftMax = 0;
-        int rightMax = 0;
+        int left = 0, right = n-1;
+        int leftMax = 0, rightMax = 0;
+        int res = 0;
         
-        int trapped = 0;
-        while(left < right) {
-            if(height[left] < height[right]) {
-                if(height[left] > leftMax)
-                    leftMax = height[left];
+        while(left <= right) {
+            if(height[left] <= height[right]) {
+                if(leftMax > height[left])
+                    res += leftMax - height[left];
                 else
-                    trapped += leftMax - height[left];
+                    leftMax = height[left];
                 left++;
             } else {
-                if(height[right] > rightMax)
-                    rightMax = height[right];
+                if(rightMax > height[right])
+                    res += rightMax - height[right];
                 else
-                    trapped += rightMax - height[right];
-                right--;   
+                    rightMax = height[right];
+                right--;
             }
         }
-        return trapped;
+        return res;
     }
 }
