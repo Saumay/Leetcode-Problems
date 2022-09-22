3123228ff1c0ac569b9fea124c565729789791ca
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-22 12:09:53 +0530

    Time: 5 ms, Memory: 46.4 MB - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,43 +1,99 @@
 class Solution {
-    public int countComponents(int n, int[][] edges) {
-        List<Integer>[] adjList = getAdjList(n, edges);
-        boolean[] visited = new boolean[n];
+    
+    // 1) DFS
+//     public int countComponents(int n, int[][] edges) {
+//         List<Integer>[] adjList = getAdjList(n, edges);
+//         boolean[] visited = new boolean[n];
         
-        int components = 0;
-        for(int i=0 ; i<n ; i++) {
-            if(!visited[i]) {
-                components++;
-                dfs(i, adjList, visited);
-            }
+//         int components = 0;
+//         for(int i=0 ; i<n ; i++) {
+//             if(!visited[i]) {
+//                 components++;
+//                 dfs(i, adjList, visited);
+//             }
+//         }
+//         return components;
+//     }
+    
+//     private void dfs(int src, List<Integer>[] adjList, boolean[] visited) {
+//         List<Integer> nbrs = adjList[src];
+        
+//         for(int nbr : nbrs) {
+//             if(!visited[nbr]) {
+//                 visited[nbr] = true;
+//                 dfs(nbr, adjList, visited);
+//             }
+//         }
+//     }
+    
+//     private List<Integer>[] getAdjList(int n, int[][] edges) {
+//         List<Integer>[] adjList = new ArrayList[n];
+        
+//         for(int i=0 ; i<n ; i++) {
+//             adjList[i] = new ArrayList<>();
+//         }
+        
+//         for(int[] edge : edges) {
+//             int src = edge[0];
+//             int dest = edge[1];
+            
+//             adjList[src].add(dest);
+//             adjList[dest].add(src);
+//         }
+//         return adjList;
+//     }
+    
+    
+    // 2) Union Find
+    public int countComponents(int n, int[][] edges) {
+        DisjointSet ds = new DisjointSet(n);
+        int components = n;
+        
+        for(int[] edge : edges) {
+            if(ds.union(edge[0], edge[1]))
+                components--;
         }
         return components;
     }
     
-    private void dfs(int src, List<Integer>[] adjList, boolean[] visited) {
-        List<Integer> nbrs = adjList[src];
+    class DisjointSet {
+        private int[] root;
+        private int[] rank;
         
-        for(int nbr : nbrs) {
-            if(!visited[nbr]) {
-                visited[nbr] = true;
-                dfs(nbr, adjList, visited);
+        DisjointSet(int n) {  
+            root = new int[n];
+            rank = new int[n];
+            
+            for(int i=0 ; i<n ; i++) {
+                root[i] = i;
+                rank[i] = 1;
             }
         }
-    }
-    
-    private List<Integer>[] getAdjList(int n, int[][] edges) {
-        List<Integer>[] adjList = new ArrayList[n];
         
-        for(int i=0 ; i<n ; i++) {
-            adjList[i] = new ArrayList<>();
-        }
-        
-        for(int[] edge : edges) {
-            int src = edge[0];
-            int dest = edge[1];
+        private int find(int x) {
+            if(root[x] == x)
+                return x;
             
-            adjList[src].add(dest);
-            adjList[dest].add(src);
+            root[x] = find(root[x]);
+            return root[x];
         }
-        return adjList;
+        
+        public boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            
+            if(rootX != rootY) {
+                if(rank[rootX] < rank[rootY])
+                    root[rootX] = rootY;
+                else if(rank[rootX] > rank[rootY])
+                    root[rootY] = rootX;
+                else {
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
