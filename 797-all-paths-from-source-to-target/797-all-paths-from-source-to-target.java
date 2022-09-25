f8a35b6f529150ac730e76d98c488109833e4cb6
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-25 21:02:59 +0530

    Time: 12 ms (15.67%), Space: 54.9 MB (65.25%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,31 +1,59 @@
 class Solution {
     
     // 1) DFS: Recursive
+//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//         int n = graph.length;
+//         List<List<Integer>> paths = new ArrayList<>();
+        
+//         int src = 0;
+//         int dest = n-1;
+        
+//         // boolean[] seen = new boolean[n];     // Not required, since it's a DAG
+//         List<Integer> path = new LinkedList<>();
+//         path.add(src);
+//         dfs(graph, src, dest, path, paths);
+//         return paths;
+//     }
+    
+//     private void dfs(int[][] graph, int src, int dest, List<Integer> path, List<List<Integer>> paths) {
+//         if(src==dest) {
+//             paths.add(new LinkedList<>(path));
+//             return;
+//         }
+        
+//         int[] nbrs = graph[src];
+//         for(int nbr : nbrs) {
+//             path.add(nbr);
+//             dfs(graph, nbr, dest, path, paths);
+//             path.remove(path.size()-1);
+//         }
+//     }
+    
+    
+    // 2) DP + Memoization
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        int n = graph.length;
-        List<List<Integer>> paths = new ArrayList<>();
-        
         int src = 0;
-        int dest = n-1;
+        int dest = graph.length-1;
         
-        // boolean[] seen = new boolean[n];     // Not required, since it's a DAG
-        List<Integer> path = new LinkedList<>();
-        path.add(src);
-        dfs(graph, src, dest, path, paths);
+        List<List<Integer>> paths = getAllPaths(graph, src, dest);
         return paths;
     }
     
-    private void dfs(int[][] graph, int src, int dest, List<Integer> path, List<List<Integer>> paths) {
+    private List<List<Integer>> getAllPaths(int[][] graph, int src, int dest) {
+        List<List<Integer>> paths = new ArrayList<>();
         if(src==dest) {
-            paths.add(new LinkedList<>(path));
-            return;
+            paths.add(new LinkedList<>());
+            paths.get(0).add(src);
+            return paths;
         }
         
         int[] nbrs = graph[src];
         for(int nbr : nbrs) {
-            path.add(nbr);
-            dfs(graph, nbr, dest, path, paths);
-            path.remove(path.size()-1);
+            paths.addAll(getAllPaths(graph, nbr, dest));
         }
+        for(List<Integer> path : paths) {
+            path.add(0, src);
+        }
+        return paths;
     }
 }
