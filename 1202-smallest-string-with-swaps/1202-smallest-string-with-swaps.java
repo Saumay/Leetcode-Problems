d021e6d8c8435ab857628dd312ab4c215b5a9427
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-25 19:07:34 +0530

    Time: 103 ms (34.76%), Space: 99.1 MB (67.91%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -0,0 +1,103 @@
+class Solution {
+    
+    // 1) Union Find
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        DisjointSet ds = new DisjointSet(s.length());
+        
+        for(List<Integer> pair : pairs) {
+            ds.union(pair.get(0), pair.get(1));
+        }
+        
+        Map<Integer, List<Integer>> idxMap = new HashMap<>();
+        Map<Integer, List<Character>> charMap = new HashMap<>();
+        
+        for(int i=0 ; i<s.length() ; i++) {
+            int root = ds.find(i);
+            
+            idxMap.putIfAbsent(root, new ArrayList<>());
+            idxMap.get(root).add(i);
+            
+            charMap.putIfAbsent(root, new ArrayList<>());
+            charMap.get(root).add(s.charAt(i));
+        }
+        
+        char[] result = new char[s.length()];
+        for(int key : idxMap.keySet()) {
+            List<Integer> idxs = idxMap.get(key);
+            List<Character> chars = charMap.get(key);
+            
+            Collections.sort(chars);
+            for(int i=0 ; i<idxs.size() ; i++) {
+                result[idxs.get(i)] = chars.get(i);
+            }
+        }
+        return new String(result);
+    }
+    
+    private List<String> printVal(Set<int[]> set) {
+        return set.stream()
+            .map(i1 -> i1[0] + " " + i1[1])
+            .collect(Collectors.toList());
+    }
+    
+    class DisjointSet {
+        int[] root;
+        int[] rank;
+        
+        DisjointSet(int n) {
+            this.root = new int[n];
+            this.rank = new int[n];
+            
+            for(int i=0 ; i<n ; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            if(x==root[x])
+                return x;
+            
+            root[x] = find(root[x]);
+            return root[x];
+        }
+        
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            
+            if(rootX != rootY) {
+                if(rank[rootX] < rank[rootY])
+                    root[rootX] = rootY;
+                else if(rank[rootX] > rank[rootY])
+                    root[rootY] = rootX;
+                else {
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+    }
+    
+    
+    // 2) DFS
+//     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+//         List<Integer>[] adjList = getAdjList(s.length(), pairs);
+        
+//         List<Integer> idxList = new ArrayList<>();
+//         List<Character> charList = new 
+//     }
+    
+//     private List<Integer>[] getAdjList(int n, List<List<Integer>> pairs) {
+//         List<Integer>[] adjList = new ArrayList<Integer>[n];
+        
+//         for(List<Integer> pair : pairs) {
+//             int src = pair.get(0);
+//             int dest = pair.get(1);
+            
+//             adjList[src].add(dest);
+//             adjList[dest].add(src);
+//         }
+//         return adjList;
+//     }
+}
