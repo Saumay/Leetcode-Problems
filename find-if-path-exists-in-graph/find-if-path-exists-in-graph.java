fbd714a77d63f722df43055404927ff3fe23f798
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-25 19:52:41 +0530

    Time: 156 ms, Memory: 188.3 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,23 +1,53 @@
 class Solution {
+    
+    // 1) DFS - Recursive
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         List<Integer>[] adjList = getAdjList(n, edges);
+//         boolean[] seen = new boolean[n];
+        
+//         seen[source] = true;
+//         return dfs(adjList, source, destination, seen);
+//     }
+    
+//     private boolean dfs(List<Integer>[] adjList, int src, int dest, boolean[] seen) {
+//         if(src==dest)
+//             return true;
+        
+//         List<Integer> nbrs = adjList[src];
+        
+//         for(int nbr : nbrs) {
+//             if(!seen[nbr]) {
+//                 seen[nbr] = true;
+//                 if(dfs(adjList, nbr, dest, seen))
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+    
+    
+    // 2) DFS - Iterative
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<Integer>[] adjList = getAdjList(n, edges);
         boolean[] seen = new boolean[n];
         
         seen[source] = true;
-        return dfs(adjList, source, destination, seen);
-    }
-    
-    private boolean dfs(List<Integer>[] adjList, int src, int dest, boolean[] seen) {
-        if(src==dest)
-            return true;
+        Deque<Integer> st = new ArrayDeque<>();
+        st.push(source);
         
-        List<Integer> nbrs = adjList[src];
-        
-        for(int nbr : nbrs) {
-            if(!seen[nbr]) {
-                seen[nbr] = true;
-                if(dfs(adjList, nbr, dest, seen))
-                    return true;
+        while(!st.isEmpty()) {
+            // Remove
+            int top = st.pop();
+            
+            if(top==destination)
+                return true;
+            
+            // Add
+            for(int nbr : adjList[top]) {
+                if(!seen[nbr]) {
+                    seen[nbr] = true;
+                    st.push(nbr);
+                }
             }
         }
         return false;
