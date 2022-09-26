9945c230caacd26497d6618762c04edf91f44ea9
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-27 00:12:46 +0530

    Time: 22 ms (30.26%), Space: 54.7 MB (58.02%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.java
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.java
@@ -1,52 +1,23 @@
 class Solution {
-    
-    // We don't use the state WHITE as such anywhere. Instead, the "null" value in the states array below is a substitute for WHITE.
-    enum Color { GRAY, BLACK };
-    
     public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
-        
-        List<Integer>[] graph = buildDigraph(n, edges);
-        return leadsToDest(graph, source, destination, new Color[n]);
-    }
-    
-    private boolean leadsToDest(List<Integer>[] graph, int node, int dest, Color[] states) {
-        
-        // If the state is GRAY, this is a backward edge and hence, it creates a loop.
-        if (states[node] != null) {
-            return states[node] == Color.BLACK;
-        }
-        
-        // If this is a leaf node, it should be equal to the destination.
-        if (graph[node].isEmpty()) {
-            return node == dest;
-        }
-        
-        // Now, we are processing this node. So we mark it as GRAY
-        states[node] = Color.GRAY;
-        
-        for (int next : graph[node]) {
-            
-            // If we get a `false` from any recursive call on the neighbors, we short circuit and return from there.
-            if (!leadsToDest(graph, next, dest, states)) {
-                return false;
-            }
-        }
-        
-        // Recursive processing done for the node. We mark it BLACK
-        states[node] = Color.BLACK;
-        return true;
-    }
-    
-    private List<Integer>[] buildDigraph(int n, int[][] edges) {
-        List<Integer>[] graph = new List[n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
+        List<Integer>[] adjList = new ArrayList[n];
         for (int[] edge : edges) {
-            graph[edge[0]].add(edge[1]);
+            if (adjList[edge[0]] == null) adjList[edge[0]] = new ArrayList<>();
+            adjList[edge[0]].add(edge[1]);
         }
-        
-        return graph;
+        return dfs(source, destination, adjList, new boolean[n], new boolean[n]);
+    }
+
+    private boolean dfs(int source, int dest, List<Integer>[] adjList, boolean[] visiting, boolean[] visited) {
+        if (adjList[source] == null) return source == dest;
+
+        if (visited[source]) return true;
+        if (visiting[source]) return false;
+        visiting[source] = true;
+        for (int neighbour : adjList[source])
+            if (!dfs(neighbour, dest, adjList, visiting, visited)) return false;
+        visiting[source] = false;
+        visited[source] = true;
+        return true;
     }
 }
