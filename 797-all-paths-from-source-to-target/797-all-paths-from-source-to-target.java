391ee779538c48777d40c6bd8b8ae00800535641
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-27 11:50:01 +0530

    Time: 27 ms (5.42%), Space: 54.8 MB (75.24%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -58,37 +58,68 @@ class Solution {
     
     
     // 3) DP: Recursion + Memoization
-    private Map<Integer, List<List<Integer>>> cache = new HashMap<>();
+//     private Map<Integer, List<List<Integer>>> cache = new HashMap<>();
     
+//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//         int src = 0;
+//         int dest = graph.length-1;
+        
+//         return getAllPaths(graph, src, dest);
+//     }
+    
+//     private List<List<Integer>> getAllPaths(int[][] graph, int src, int dest) {
+//         List<List<Integer>> paths = new ArrayList<>();
+//         if(src==dest) {
+//             paths.add(new LinkedList<>());
+//             paths.get(0).add(src);
+//             return paths;
+//         }
+//         if(cache.containsKey(src)) {
+//             return cache.get(src);
+//         }
+        
+//         int[] nbrs = graph[src];
+//         for(int nbr : nbrs) {
+//             List<List<Integer>> nbrPaths = getAllPaths(graph, nbr, dest);
+//             for(List<Integer> nbrPath : nbrPaths) {
+//                 List<Integer> newPath = new LinkedList<>(nbrPath);
+//                 newPath.add(0, src);
+//                 paths.add(newPath);
+//             }
+//         }
+        
+//         cache.put(src, paths);
+//         return paths;
+//     }
+    
+    
+    // 3) BFS
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         int src = 0;
         int dest = graph.length-1;
         
-        return getAllPaths(graph, src, dest);
-    }
-    
-    private List<List<Integer>> getAllPaths(int[][] graph, int src, int dest) {
-        List<List<Integer>> paths = new ArrayList<>();
-        if(src==dest) {
-            paths.add(new LinkedList<>());
-            paths.get(0).add(src);
-            return paths;
-        }
-        if(cache.containsKey(src)) {
-            return cache.get(src);
-        }
+        // Set<Integer> seen = new HashSet<>();        // Not required, since it's a DAG
+        List<List<Integer>> paths = new LinkedList<>();
+        Queue<List<Integer>> q = new ArrayDeque<>();
+        List<Integer> path = new LinkedList<>();
+        path.add(src);
+        q.add(path);
         
-        int[] nbrs = graph[src];
-        for(int nbr : nbrs) {
-            List<List<Integer>> nbrPaths = getAllPaths(graph, nbr, dest);
-            for(List<Integer> nbrPath : nbrPaths) {
-                List<Integer> newPath = new LinkedList<>(nbrPath);
-                newPath.add(0, src);
-                paths.add(newPath);
+        while(!q.isEmpty()) {
+            List<Integer> srcPath = q.remove();
+            
+            int cur = srcPath.get(srcPath.size()-1);
+            if(cur == dest) {
+                paths.add(srcPath);
+                continue;
+            }
+            
+            for(int nbr : graph[cur]) {      
+                List<Integer> nbrPath = new LinkedList<>(srcPath);
+                nbrPath.add(nbr);
+                q.add(nbrPath);
             }
         }
-        
-        cache.put(src, paths);
         return paths;
     }
 }
