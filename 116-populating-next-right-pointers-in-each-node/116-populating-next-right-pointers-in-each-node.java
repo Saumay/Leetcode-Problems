fa163d04e183556b6cf397e783a5ee5d81fb1914
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-27 12:27:09 +0530

    Time: 5 ms (28.14%), Space: 47.5 MB (43.09%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,86 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+//     public Node connect(Node root) {
+//         if(root==null)
+//             return null;
+        
+//         Queue<Node> q = new ArrayDeque<>();
+//         q.add(root);
+        
+//         while(!q.isEmpty()) {            
+//             int size = q.size();
+//             Node head = null;
+            
+//             for(int i=0 ; i<size ; i++) {
+//                 // R - Remove
+//                 Node cur = q.remove();
+                
+//                 // A - Add
+//                 if(cur.left != null)
+//                     q.add(cur.left);
+//                 if(cur.right != null)
+//                     q.add(cur.right);
+                
+//                 // P - Processing
+//                 if(head == null)
+//                     head = cur;
+//                 else {
+//                     head.next = cur;
+//                     head = head.next;
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+    
+    
+    // 2) Using previously established pointers
+    public Node connect(Node root) {
+        if(root==null)
+            return null;
+        
+        Node prevLevel = root;
+        Node curLevel = root.left;
+        
+        while(curLevel != null) {
+            Node prevLevelHead = prevLevel;
+            Node curLevelHead = new Node(-1);
+            
+            while(prevLevelHead != null) {
+                curLevelHead.next = prevLevelHead.left;
+                curLevelHead = curLevelHead.next;
+                curLevelHead.next = prevLevelHead.right;
+                curLevelHead = curLevelHead.next;
+                
+                prevLevelHead = prevLevelHead.next;
+            }
+            System.out.println();
+            
+            prevLevel = curLevel;
+            curLevel = curLevel.left;
+        }
+        return root;
+    }
+}
