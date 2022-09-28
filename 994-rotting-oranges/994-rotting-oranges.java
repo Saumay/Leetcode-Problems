a873436de282ea4507092ccbae8208f5cf6334c6
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-28 16:43:19 +0530

    Time: 6 ms (16.76%), Space: 42.9 MB (56.59%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,5 +1,11 @@
 class Solution {
+    
+    private int[][] dirs = new int[][] {{1,0}, {0,1}, {-1,0}, {0,-1}};
+    
     public int orangesRotting(int[][] grid) {
+        if(grid==null || grid.length==0)
+            return -1;
+        
         int m = grid.length;
         int n = grid[0].length;
         
@@ -8,39 +14,43 @@ class Solution {
         for(int i=0 ; i<m ; i++) {
             for(int j=0 ; j<n ; j++) {
                 if(grid[i][j] == 2)
-                    q.add(new int[] {i, j});
-                
-                if(grid[i][j] == 1)
+                    q.add(new int[] {i,j});
+                else if(grid[i][j] == 1)
                     fresh++;
             }
         }
         
         if(fresh==0)
             return 0;
-        if(q.isEmpty())
-            return -1;
         
-        int[][] dirs = new int[][] {{0,-1}, {-1,0}, {1,0}, {0,1}};        
-        int time=-1;
+        int time=0;
         while(!q.isEmpty()) {
             int size = q.size();
-            
+                       
+            time++;
             for(int i=0 ; i<size ; i++) {
-                int[] cur = q.remove();
+                int[] cell = q.remove();
                 
                 for(int[] dir : dirs) {
-                    int nbrI = cur[0] + dir[0];
-                    int nbrJ = cur[1] + dir[1];
-
-                    if(nbrI>=0 && nbrI<m && nbrJ>=0 && nbrJ<n && grid[nbrI][nbrJ]==1) {
+                    int nbrI = cell[0] + dir[0];
+                    int nbrJ = cell[1] + dir[1];
+                    
+                    if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && grid[nbrI][nbrJ]==1) {
+                        fresh--;
+                        if(fresh==0)
+                            return time;
+                        
                         grid[nbrI][nbrJ] = 2;
                         q.add(new int[] {nbrI, nbrJ});
-                        fresh--;
                     }
                 }
             }
-            time++;
+            // time++;
         }
-        return fresh>0 ? -1 : time;
+        return -1;
+    }
+    
+    private String stringify(int x, int y) {
+        return String.valueOf(x) + "," + String.valueOf(y);
     }
 }
