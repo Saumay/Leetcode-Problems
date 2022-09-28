92185f0a84c7453084f9f01f5b9a4364387adb57
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-28 16:37:29 +0530

    Time: 28 ms, Memory: 45 MB - LeetHub

---
+++ rotting-oranges/rotting-oranges.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    private int[][] dirs = new int[][] {{1,0}, {0,1}, {-1,0}, {0,-1}};
+    
+    public int orangesRotting(int[][] grid) {
+        if(grid==null || grid.length==0)
+            return -1;
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        Set<String> fresh = new HashSet<>();
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int i=0 ; i<m ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                if(grid[i][j] == 2)
+                    q.add(new int[] {i,j});
+                else if(grid[i][j] == 1)
+                    fresh.add(stringify(new int[] {i,j}));
+            }
+        }
+        if(fresh.isEmpty())
+            return 0;
+        
+        int time=0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+                                    
+            for(int i=0 ; i<size ; i++) {
+                int[] cell = q.remove();
+                
+                fresh.remove(stringify(cell));
+                
+                // fresh.stream().forEach(cc -> System.out.print(cc[0] + "," + cc[1] + " "));
+                // System.out.println("----" + cell[0] + "," + cell[1]);
+                
+                if(fresh.isEmpty())
+                    return time;
+                
+                for(int[] dir : dirs) {
+                    int nbrI = cell[0] + dir[0];
+                    int nbrJ = cell[1] + dir[1];
+                    
+                    if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && grid[nbrI][nbrJ]==1) {
+                        grid[nbrI][nbrJ] = 2;
+                        q.add(new int[] {nbrI, nbrJ});
+                    }
+                }
+            }
+            time++;
+        }
+        return -1;
+    }
+    
+    private String stringify(int[] cell) {
+        return String.valueOf(cell[0]) + "," + String.valueOf(cell[1]);
+    }
+}
