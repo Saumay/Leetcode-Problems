b1225dc65cd37f1b8dcba61551cdd4e864ec8038
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-29 19:58:15 +0530

    Time: 1 ms (91.16%), Space: 48.1 MB (48.38%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -2,28 +2,51 @@ class Solution {
     
     private int[][] dirs = new int[][] {{1,0}, {0,1}, {-1,0}, {0,-1}};
     
+//     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+//         // DFS
+//         if(image[sr][sc]==color)
+//             return image;
+        
+//         dfs(image, sr, sc, image[sr][sc], color);
+//         image[sr][sc] = color;
+//         return image;
+//     }
+    
+    
+    // 1.1) DFS 1
+//     private void dfs(int[][] image, int sr, int sc, int origColor, int newColor) {
+//         int m = image.length;
+//         int n = image[0].length;
+        
+//         for(int[] dir : dirs) {
+//             int nbrI = sr+dir[0];
+//             int nbrJ = sc+dir[1];
+            
+//             if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && image[nbrI][nbrJ]==origColor) {
+//                 image[nbrI][nbrJ] = newColor;
+//                 dfs(image, nbrI, nbrJ, origColor, newColor);
+//             }
+//         }
+//     }
+    
+    
+    // 1.2) DFS 2
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         // DFS
         if(image[sr][sc]==color)
             return image;
         
         dfs(image, sr, sc, image[sr][sc], color);
-        image[sr][sc] = color;
         return image;
     }
     
     private void dfs(int[][] image, int sr, int sc, int origColor, int newColor) {
-        int m = image.length;
-        int n = image[0].length;
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=origColor)
+            return;
         
+        image[sr][sc] = newColor;
         for(int[] dir : dirs) {
-            int nbrI = sr+dir[0];
-            int nbrJ = sc+dir[1];
-            
-            if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && image[nbrI][nbrJ]==origColor) {
-                image[nbrI][nbrJ] = newColor;
-                dfs(image, nbrI, nbrJ, origColor, newColor);
-            }
+            dfs(image, sr+dir[0], sc+dir[1], origColor, newColor);
         }
     }
 }
