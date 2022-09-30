3595c5e9af1395dbc3c1adda012d81399563efd1
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-30 19:18:39 +0530

    Time: 41 ms (17.79%), Space: 74.4 MB (19.51%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -6,41 +6,37 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         
-        int[][] res = new int[m][n];
-        boolean[][] seen = new boolean[m][n];
-        
+        int[][] dist = new int[m][n];        
         Queue<int[]> q = new ArrayDeque<>();
         
         for(int i=0 ; i<m ; i++) {
             for(int j=0 ; j<n ; j++) {
                 if(mat[i][j]==0) {
+                    dist[i][j] = 0;
                     q.add(new int[]{i,j});
-                    seen[i][j] = true;
+                } else {
+                    dist[i][j] = -1;
                 }
             }
         }
         
-        int dist=0;
         while(!q.isEmpty()) {
             int size = q.size();
             
             for(int i=0 ; i<size ; i++) {
                 int[] cell = q.remove();
                 
-                res[cell[0]][cell[1]] = dist;
-                
                 for(int[] dir : dirs) {
                     int nbrI = cell[0] + dir[0];
                     int nbrJ = cell[1] + dir[1];
                     
-                    if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && !seen[nbrI][nbrJ]) {
-                        seen[nbrI][nbrJ] = true;
+                    if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && dist[nbrI][nbrJ]==-1) {
+                        dist[nbrI][nbrJ] = dist[cell[0]][cell[1]] + 1;
                         q.add(new int[] {nbrI, nbrJ});
                     }
                 }
             }
-            dist++;
         }
-        return res;
+        return dist;
     }
 }
