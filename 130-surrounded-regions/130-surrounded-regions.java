6a0dcdf989026fa9626647f5ae6b251ad2d06f27
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-30 21:21:00 +0530

    Time: 1 ms (99.92%), Space: 44.5 MB (89.91%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -86,45 +86,4 @@ class Solution {
             flip(board, i+dir[0], j+dir[1]);
         }
     }
-    
-    
-//     public void solve(char[][] board) {
-//         int m = board.length;
-//         int n = board[0].length;
-        
-//         for(int i=1 ; i<m-1 ; i++) {
-//             for(int j=1 ; j<n-1 ; j++) {
-//                 if(board[i][j] == 'O') {
-//                     board[i][j] = 'X';
-                    
-//                     if(!flip(board, i, j)) {
-//                         board[i][j] = 'O';
-//                     }
-//                 }
-//             }
-//         }
-//     }
-    
-//     private boolean flip(char[][] board, int i, int j) {
-//         int m = board.length;
-//         int n = board[0].length;
-        
-//         if(i==0 || j==0 || i==m-1 || j==n-1)
-//             return false;
-        
-//         for(int[] dir : dirs) {
-//             int nbrI = i+dir[0];
-//             int nbrJ = j+dir[1];
-            
-//             if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && board[nbrI][nbrJ]=='O') {
-//                 board[nbrI][nbrJ] = 'X';
-                
-//                 if(!flip(board, nbrI, nbrJ)) {
-//                     board[nbrI][nbrJ] = 'O';
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
 }
