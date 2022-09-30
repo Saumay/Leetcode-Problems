adc1dd17a24a4d4ed934542cd7153caeab91791c
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-09-30 22:01:10 +0530

    Time: 1 ms (99.92%), Space: 44.2 MB (97.59%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,45 +1,45 @@
 class Solution {
     
     // 1) DFS
-//     private int[][] dirs = new int[][] {{1,0},{0,1},{-1,0},{0,-1}};
+    private int[][] dirs = new int[][] {{1,0},{0,1},{-1,0},{0,-1}};
     
-//     public void solve(char[][] board) {
-//         int m = board.length;
-//         int n = board[0].length;
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
         
-//         for(int i=0 ; i<m ; i++) { 
-//             dfs(board, i, 0);
-//             dfs(board, i, n-1);
-//         }
+        for(int i=0 ; i<m ; i++) { 
+            dfs(board, i, 0);
+            dfs(board, i, n-1);
+        }
         
-//         for(int j=0 ; j<n ; j++) {
-//             dfs(board, 0, j);
-//             dfs(board, m-1, j);
-//         }
+        for(int j=0 ; j<n ; j++) {
+            dfs(board, 0, j);
+            dfs(board, m-1, j);
+        }
         
-//         for(int i=0 ; i<m ; i++) {
-//             for(int j=0 ; j<n ; j++) {
-//                 if(board[i][j] == 'O')
-//                     board[i][j] = 'X';
-//                 else if(board[i][j] == '-')
-//                     board[i][j] = 'O';
-//             }
-//         }
-//     }
+        for(int i=0 ; i<m ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == '-')
+                    board[i][j] = 'O';
+            }
+        }
+    }
     
-//     private void dfs(char[][] board, int i, int j) {
-//         if(board[i][j] != 'O')
-//             return;
+    private void dfs(char[][] board, int i, int j) {
+        if(board[i][j] != 'O')
+            return;
         
-//         board[i][j] = '-';
-//         for(int[] dir : dirs) {
-//             int nbrI = dir[0] + i;
-//             int nbrJ = dir[1] + j;
+        board[i][j] = '-';
+        for(int[] dir : dirs) {
+            int nbrI = dir[0] + i;
+            int nbrJ = dir[1] + j;
             
-//             if(nbrI>=0 && nbrJ>=0 && nbrI<board.length && nbrJ<board[0].length)
-//                 dfs(board, i+dir[0], j+dir[1]);
-//         }
-//     }
+            if(nbrI>=0 && nbrJ>=0 && nbrI<board.length && nbrJ<board[0].length)
+                dfs(board, i+dir[0], j+dir[1]);
+        }
+    }
     
     
     // 2) BFS
@@ -97,85 +97,85 @@ class Solution {
     
     
     // 3) Union Find
-    private int[][] dirs = new int[][] {{0,1}, {1,0}, {0,-1}, {-1,0}};
+//     private int[][] dirs = new int[][] {{0,1}, {1,0}, {0,-1}, {-1,0}};
     
-    public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
+//     public void solve(char[][] board) {
+//         int m = board.length;
+//         int n = board[0].length;
         
-        DisjointSet ds = new DisjointSet(m*n+1);
-        int boundary = m*n;
+//         DisjointSet ds = new DisjointSet(m*n+1);
+//         int boundary = m*n;
         
-        for(int i=0 ; i<m ; i++) {
-            for(int j=0 ; j<n ; j++) {
-                if(board[i][j] == 'O') {
-                    if(isBorder(i,j,m,n))
-                        ds.union(boundary, getId(i, j, n));
-                    else {
-                        for(int[] dir : dirs) {
-                            int nbrI = i+dir[0];
-                            int nbrJ = j+dir[1];
+//         for(int i=0 ; i<m ; i++) {
+//             for(int j=0 ; j<n ; j++) {
+//                 if(board[i][j] == 'O') {
+//                     if(isBorder(i,j,m,n))
+//                         ds.union(boundary, getId(i, j, n));
+//                     else {
+//                         for(int[] dir : dirs) {
+//                             int nbrI = i+dir[0];
+//                             int nbrJ = j+dir[1];
                             
-                            if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && board[nbrI][nbrJ]=='O')
-                                ds.union(getId(i,j,n), getId(nbrI,nbrJ,n));
-                        }
-                    }
-                }
-            }
-        }
+//                             if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n && board[nbrI][nbrJ]=='O')
+//                                 ds.union(getId(i,j,n), getId(nbrI,nbrJ,n));
+//                         }
+//                     }
+//                 }
+//             }
+//         }
         
-        for(int i=0 ; i<m ; i++) {
-            for(int j=0 ; j<n ; j++) {
-                if(ds.find(boundary) != ds.find(getId(i,j,n)))
-                    board[i][j] = 'X';
-            }
-        }
-    }
+//         for(int i=0 ; i<m ; i++) {
+//             for(int j=0 ; j<n ; j++) {
+//                 if(ds.find(boundary) != ds.find(getId(i,j,n)))
+//                     board[i][j] = 'X';
+//             }
+//         }
+//     }
     
-    private boolean isBorder(int i, int j, int m, int n) {
-        return i==0 || j==0 || i==m-1 || j==n-1;
-    }
+//     private boolean isBorder(int i, int j, int m, int n) {
+//         return i==0 || j==0 || i==m-1 || j==n-1;
+//     }
     
-    private int getId(int i, int j, int n) {
-        return n*i + j;
-    }
+//     private int getId(int i, int j, int n) {
+//         return n*i + j;
+//     }
     
-    class DisjointSet {
-        int[] root;
-        int[] rank;
+//     class DisjointSet {
+//         int[] root;
+//         int[] rank;
         
-        public DisjointSet(int n) {
-            root = new int[n];
-            rank = new int[n];
+//         public DisjointSet(int n) {
+//             root = new int[n];
+//             rank = new int[n];
             
-            for(int i=0 ; i<n ; i++) {
-                root[i] = i;
-                rank[i] = 1;
-            }
-        }
+//             for(int i=0 ; i<n ; i++) {
+//                 root[i] = i;
+//                 rank[i] = 1;
+//             }
+//         }
         
-        public int find(int x) {
-            if(root[x]==x)
-                return x;
+//         public int find(int x) {
+//             if(root[x]==x)
+//                 return x;
             
-            root[x] = find(root[x]);
-            return root[x];
-        }
+//             root[x] = find(root[x]);
+//             return root[x];
+//         }
         
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
+//         public void union(int x, int y) {
+//             int rootX = find(x);
+//             int rootY = find(y);
             
-            if(rootX != rootY) {
-                if(rank[rootX] < rank[rootY])
-                    root[rootX] = rootY;
-                else if(rank[rootX] > rank[rootY])
-                    root[rootY] = rootX;
-                else {
-                    root[rootY] = rootX;
-                    rank[rootX]++;
-                }
-            }
-        }
-    }
+//             if(rootX != rootY) {
+//                 if(rank[rootX] < rank[rootY])
+//                     root[rootX] = rootY;
+//                 else if(rank[rootX] > rank[rootY])
+//                     root[rootY] = rootX;
+//                 else {
+//                     root[rootY] = rootX;
+//                     rank[rootX]++;
+//                 }
+//             }
+//         }
+//     }
 }
