a12bfb56e40d791be89c56f7373226e770718ee0
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-01 21:38:58 +0530

    Time: 139 ms (11.11%), Space: 173 MB (11.11%) - LeetHub

---
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
@@ -0,0 +1,79 @@
+class LUPrefix {
+
+    Set<Integer> set;
+    int max = 0;
+    public LUPrefix(int n) {
+        set = new HashSet(n);
+    }
+    
+    public void upload(int video) {
+        set.add(video);
+        while(set.contains(max+1))max++;
+    }
+    
+    public int longest() {
+        return max;
+    }
+    
+    
+//     Set<Integer> seen;
+//     DisjointSet ds;
+    
+//     public LUPrefix(int n) {
+//         seen = new HashSet<>();
+//         seen.add(0);
+        
+//         ds = new DisjointSet(n+1);
+//     }
+    
+//     public void upload(int video) {
+//         seen.add(video);
+//         if(seen.contains(video-1))
+//             ds.union(video-1, video);
+//         if(seen.contains(video+1))
+//             ds.union(video, video+1);
+//     }
+    
+//     public int longest() {
+//         return ds.find(0);
+//     }
+    
+//     class DisjointSet {
+//         int[] root;
+//         int[] rank;
+        
+//         public DisjointSet(int n) {
+//             root = new int[n+1];
+//             rank = new int[n+1];
+            
+//             for(int i=0 ; i<n+1 ; i++) {
+//                 root[i] = i;
+//                 rank[i] = 1;
+//             }
+//         }
+        
+//         public void union(int x, int y) {
+//             int rootX = find(x);
+//             int rootY = find(y);
+            
+//             if(rootX != rootY) {
+//                 root[rootX] = rootY;
+//             }
+//         }
+        
+//         public int find(int x) {
+//             if(root[x]==x)
+//                 return root[x];
+            
+//             root[x] = find(root[x]);
+//             return root[x];
+//         }
+//     }
+}
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix obj = new LUPrefix(n);
+ * obj.upload(video);
+ * int param_2 = obj.longest();
+ */
