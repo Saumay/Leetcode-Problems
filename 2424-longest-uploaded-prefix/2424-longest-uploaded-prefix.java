5a32a158df336f7f52db22aaefca004cd2562336
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-02 01:18:39 +0530

    Time: 224 ms (11.11%), Space: 165.1 MB (11.11%) - LeetHub

--- 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
@@ -26,60 +26,81 @@ class LUPrefix {
     
     
     // 2) Using Set + DisjointSet
-    private Set<Integer> seen;
-    private DisjointSet ds;
+//     private Set<Integer> seen;
+//     private DisjointSet ds;
+    
+//     public LUPrefix(int n) {
+//         seen = new HashSet<>();
+//         seen.add(0);
+        
+//         ds = new DisjointSet(n+1);
+//     }
+    
+//     public void upload(int video) {
+//         seen.add(video);
+        
+//         if(seen.contains(video-1))
+//             ds.union(video-1, video);
+//         if(seen.contains(video+1))
+//             ds.union(video, video+1);
+//     }
+    
+//     public int longest() {
+//         return ds.find(0);
+//     }
+    
+    
+//     class DisjointSet {
+//         int[] root;
+
+//         public DisjointSet(int n) {
+//             root = new int[n+1];
+
+//             for(int i=0 ; i<n+1 ; i++) {
+//                 root[i] = i;
+//             }
+//         }
+
+//         public void union(int x, int y) {
+//             int rootX = find(x);
+//             int rootY = find(y);
+
+//             if(rootX != rootY) {
+//                 if(rootX > rootY)
+//                     root[rootY] = rootX;
+//                 else
+//                     root[rootX] = rootY;
+//             }
+//         }
+
+//         public int find(int x) {
+//             if(root[x]==x)
+//                 return root[x];
+
+//             root[x] = find(root[x]);
+//             return root[x];
+//         }
+//     }
+    
+    
+    // 3) 
+    private TreeSet<Integer> set;
+    private int n;
     
     public LUPrefix(int n) {
-        seen = new HashSet<>();
-        seen.add(0);
+        this.set = new TreeSet<>();
+        this.n = n;
         
-        ds = new DisjointSet(n+1);
+        for(int i=1 ; i<=n ; i++)
+            set.add(i);
     }
     
     public void upload(int video) {
-        seen.add(video);
-        
-        if(seen.contains(video-1))
-            ds.union(video-1, video);
-        if(seen.contains(video+1))
-            ds.union(video, video+1);
+        set.remove(video);
     }
     
     public int longest() {
-        return ds.find(0);
-    }
-    
-    
-    class DisjointSet {
-        int[] root;
-
-        public DisjointSet(int n) {
-            root = new int[n+1];
-
-            for(int i=0 ; i<n+1 ; i++) {
-                root[i] = i;
-            }
-        }
-
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-
-            if(rootX != rootY) {
-                if(rootX > rootY)
-                    root[rootY] = rootX;
-                else
-                    root[rootX] = rootY;
-            }
-        }
-
-        public int find(int x) {
-            if(root[x]==x)
-                return root[x];
-
-            root[x] = find(root[x]);
-            return root[x];
-        }
+        return set.isEmpty() ? n : set.first()-1;
     }
 }
 
