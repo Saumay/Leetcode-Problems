898f0b740ad4f5a7bd94f095049cbfa21833d61d
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-01 18:53:25 +0530

    Time: 9 ms (83.23%), Space: 42.7 MB (86.77%) - LeetHub

---
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.java
@@ -0,0 +1,41 @@
+class Solution {
+    
+    private int[][] dirs = new int[][] {{1,0},{0,1},{0,-1},{-1,0}};
+    
+    public int numDistinctIslands(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        Set<String> islands = new HashSet<>();
+        boolean[][] visited = new boolean[m][n];
+        
+        for(int i=0 ; i<m ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                if(grid[i][j] == 1 && !visited[i][j]) {
+                    StringBuilder cells = new StringBuilder();
+                    
+                    visited[i][j] = true;
+                    cells.append(0).append("-").append(0).append(",");
+                    dfs(grid, i, j, i, j, cells, visited);
+                    islands.add(cells.toString());
+                }
+            }
+        }
+        return islands.size();
+    }
+    
+    private void dfs(int[][] grid, int srcI, int srcJ, int i, int j, StringBuilder cells, boolean[][] visited) {
+        
+        for(int[] dir : dirs) {
+            int nbrI = i+dir[0];
+            int nbrJ = j+dir[1];
+            
+            if(nbrI>=0 && nbrJ>=0 && nbrI<grid.length && nbrJ<grid[0].length && grid[nbrI][nbrJ]==1 && !visited[nbrI][nbrJ]) {
+                visited[nbrI][nbrJ] = true;
+                cells.append(nbrI-srcI).append("-").append(nbrJ-srcJ).append(",");
+                
+                dfs(grid, srcI, srcJ, nbrI, nbrJ, cells, visited);
+            }
+        }
+    }
+}
