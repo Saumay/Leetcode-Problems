37c7701e08b2e47fa3ebe1a5ed0670f301855095
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-02 01:11:01 +0530

    Time: 172 ms (11.11%), Space: 179 MB (11.11%) - LeetHub

--- 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
@@ -1,85 +1,86 @@
 class LUPrefix {
     
-    private boolean[] seen;
-    private int max;
-    
-    public LUPrefix(int n) {
-        seen = new boolean[n+1];
-        seen[0] = true;
-    }
-    
-    public void upload(int video) {
-        seen[video] = true;
-        
-        for(int i=0 ; i<seen.length ; i++) {
-            if(seen[i])
-                max = i;
-            else
-                break;
-        }
-    }
-    
-    public int longest() {
-        return max;
-    }
-    
-    
-    // 2) Using Set + DisjointSet
-//     private Set<Integer> seen;
-//     private DisjointSet ds;
+    // 1) Using array + max variable
+//     private boolean[] seen;
+//     private int max;
     
 //     public LUPrefix(int n) {
-//         seen = new HashSet<>();
-//         seen.add(0);
-        
-//         ds = new DisjointSet(n+1);
+//         seen = new boolean[n+1];
+//         seen[0] = true;
 //     }
     
 //     public void upload(int video) {
-//         seen.add(video);
+//         seen[video] = true;
         
-//         if(seen.contains(video-1))
-//             ds.union(video-1, video);
-//         if(seen.contains(video+1))
-//             ds.union(video, video+1);
+//         for(int i=0 ; i<seen.length ; i++) {
+//             if(seen[i])
+//                 max = i;
+//             else
+//                 break;
+//         }
 //     }
     
 //     public int longest() {
-//         return ds.find(0);
+//         return max;
 //     }
     
     
-//     class DisjointSet {
-//         int[] root;
+    // 2) Using Set + DisjointSet
+    private Set<Integer> seen;
+    private DisjointSet ds;
+    
+    public LUPrefix(int n) {
+        seen = new HashSet<>();
+        seen.add(0);
+        
+        ds = new DisjointSet(n+1);
+    }
+    
+    public void upload(int video) {
+        seen.add(video);
+        
+        if(seen.contains(video-1))
+            ds.union(video-1, video);
+        if(seen.contains(video+1))
+            ds.union(video, video+1);
+    }
+    
+    public int longest() {
+        return ds.find(0);
+    }
+    
+    
+    class DisjointSet {
+        int[] root;
 
-//         public DisjointSet(int n) {
-//             root = new int[n+1];
+        public DisjointSet(int n) {
+            root = new int[n+1];
 
-//             for(int i=0 ; i<n+1 ; i++) {
-//                 root[i] = i;
-//             }
-//         }
+            for(int i=0 ; i<n+1 ; i++) {
+                root[i] = i;
+            }
+        }
 
-//         public void union(int x, int y) {
-//             int rootX = find(x);
-//             int rootY = find(y);
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
 
-//             if(rootX != rootY) {
-//                 if(rootX > rootY)
-//                     root[rootY] = rootX;
-//                 else
-//                     root[rootX] = rootY;
-//             }
-//         }
+            if(rootX != rootY) {
+                if(rootX > rootY)
+                    root[rootY] = rootX;
+                else
+                    root[rootX] = rootY;
+            }
+        }
 
-//         public int find(int x) {
-//             if(root[x]==x)
-//                 return root[x];
+        public int find(int x) {
+            if(root[x]==x)
+                return root[x];
 
-//             root[x] = find(root[x]);
-//             return root[x];
-//         }
-//     }
+            root[x] = find(root[x]);
+            return root[x];
+        }
+    }
 }
 
 /**
