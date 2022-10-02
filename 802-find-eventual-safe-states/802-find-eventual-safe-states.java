ff103538f6e1da36bf4bc7b82ec2fd07dae249aa
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-02 14:38:22 +0530

    Time: 30 ms (41.69%), Space: 70.3 MB (57.87%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -0,0 +1,93 @@
+class Solution {
+    
+    // 1) Check all paths from a node and check if there is a cycle. Nodes which do not have a cycle are the safe nodes.
+//     public List<Integer> eventualSafeNodes(int[][] graph) {
+//         int n = graph.length;
+//         List<Integer> safeNodes = new LinkedList<>();
+//         boolean[] vis = new boolean[n];
+        
+//         for(int i=0 ; i<n ; i++) {
+//             if(!vis[i]) {
+//                 vis[i] = true;
+//                 boolean res = hasCycleDfs(graph, i, vis);                
+//                 vis[i] = false;
+                
+//                 if(!res)
+//                     safeNodes.add(i);
+//             }
+//         }
+//         return safeNodes;
+//     }
+    
+//     private boolean hasCycleDfs(int[][] graph, int src, boolean[] vis) {
+//         int[] nbrs = graph[src];
+        
+//         for(int nbr : nbrs) {
+//             if(!vis[nbr]) {
+//                 vis[nbr] = true;
+//                 boolean res = hasCycleDfs(graph, nbr, vis);                
+//                 vis[nbr] = false;
+                
+//                 if(res)
+//                     return true;
+                
+//             } else
+//                 return true;
+//         }
+//         return false;
+//     }
+    
+    
+    // 2) Add all nodes involved in a cycle in a set
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<Integer> safeNodes = new LinkedList<>();
+        Set<Integer> cycleNodes = new HashSet<>();
+        boolean[] vis = new boolean[n];
+        boolean[] pathVis = new boolean[n];
+        
+        for(int i=0 ; i<n ; i++) {
+            if(vis[i]) {
+                if(!cycleNodes.contains(i))
+                    safeNodes.add(i);
+            }
+            else {
+                vis[i] = true;
+                cycleNodes.add(i);
+                pathVis[i] = true;
+                
+                boolean res = hasCycleDfs(graph, i, vis, pathVis, cycleNodes);
+                
+                pathVis[i] = false;
+                if(!res) {
+                    cycleNodes.remove(i);
+                    safeNodes.add(i);
+                }
+            }
+        }
+        return safeNodes;
+    }
+    
+    private boolean hasCycleDfs(int[][] graph, int src, boolean[] vis, boolean[] pathVis, Set<Integer> cycleNodes) {
+        int[] nbrs = graph[src];
+        
+        for(int nbr : nbrs) {
+            if(!vis[nbr]) {
+                vis[nbr] = true;
+                cycleNodes.add(nbr);
+                pathVis[nbr] = true;
+                boolean res = hasCycleDfs(graph, nbr, vis, pathVis, cycleNodes);
+                pathVis[nbr] = false;
+                
+                if(res) {
+                    return true;
+                } else {
+                    cycleNodes.remove(nbr);
+                }
+                
+            } else if(pathVis[nbr] || cycleNodes.contains(nbr))
+                return true;
+        }
+        return false;
+    }
+}
