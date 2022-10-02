ee68eeb8ef1fa3d910613b0a04df2f873e64e347
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-02 22:43:03 +0530

    Time: 16 ms (32.37%), Space: 47.1 MB (78.01%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -30,45 +30,87 @@ class Solution {
     
     
     // 2) DFS 2: Cycle Detection in DG (Mark node and then call dfs)
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         List<Integer>[] adjList = getAdjList(numCourses, prerequisites);
+//         int[] color = new int[numCourses];
+        
+//         for(int i=0 ; i<numCourses ; i++) {
+//             if(color[i]==0) {
+//                 color[i] = 1;
+//                 if(cycleExistsDfs2(adjList, i, color))
+//                     return false;
+//                 color[i] = 2;
+//             }
+//         }
+//         return true;
+//     }
+    
+//     private boolean cycleExistsDfs2(List<Integer>[] adjList, int src, int[] color) {
+//         List<Integer> nbrs = adjList[src];
+        
+//         for(int nbr : nbrs) {
+//             if(color[nbr]==0) {
+//                 color[nbr] = 1;
+//                 if(cycleExistsDfs2(adjList, nbr, color))
+//                     return true;
+//                 color[nbr] = 2;
+//             } else if(color[nbr] == 1)
+//                 return true;
+//         }
+//         return false;
+//     }
+    
+//     private List<Integer>[] getAdjList(int n, int[][] prerequisites) {
+//         List<Integer>[] adjList = new ArrayList[n];
+        
+//         for(int i=0 ; i<n ; i++)
+//             adjList[i] = new ArrayList<>();
+        
+//         for(int[] course : prerequisites) {
+//             adjList[course[0]].add(course[1]);
+//         }
+//         return adjList;
+//     }
+    
+    
+    // 3) Kahn's Algorithm: Topological Sort
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adjList = getAdjList(numCourses, prerequisites);
-        int[] color = new int[numCourses];
-        
-        for(int i=0 ; i<numCourses ; i++) {
-            if(color[i]==0) {
-                color[i] = 1;
-                if(cycleExistsDfs2(adjList, i, color))
-                    return false;
-                color[i] = 2;
-            }
-        }
-        return true;
-    }
-    
-    private boolean cycleExistsDfs2(List<Integer>[] adjList, int src, int[] color) {
-        List<Integer> nbrs = adjList[src];
-        
-        for(int nbr : nbrs) {
-            if(color[nbr]==0) {
-                color[nbr] = 1;
-                if(cycleExistsDfs2(adjList, nbr, color))
-                    return true;
-                color[nbr] = 2;
-            } else if(color[nbr] == 1)
-                return true;
-        }
-        return false;
-    }
-    
-    private List<Integer>[] getAdjList(int n, int[][] prerequisites) {
+        int n = numCourses;
         List<Integer>[] adjList = new ArrayList[n];
+        int[] indegrees = new int[n];
+        Queue<Integer> q = new ArrayDeque<>();
+        int count = 0;
         
+        // Create adjacency list
         for(int i=0 ; i<n ; i++)
             adjList[i] = new ArrayList<>();
         
         for(int[] course : prerequisites) {
-            adjList[course[0]].add(course[1]);
+            int src = course[0];
+            int dest = course[1];
+            
+            indegrees[dest]++;
+            adjList[src].add(dest);
         }
-        return adjList;
+        
+        // Populate Queue with nodes having indegree 0
+        for(int i=0 ; i<n ; i++) {
+            if(indegrees[i]==0)
+                q.add(i);
+        }
+        
+        // Start removing from queue, and reducing indegree
+        while(!q.isEmpty()) {
+            int cur = q.remove();
+            count++;
+            
+            for(int nbr : adjList[cur]) {
+                indegrees[nbr]--;
+                
+                if(indegrees[nbr]==0)
+                    q.add(nbr);
+            }
+        }
+        return count==numCourses;
     }
 }
