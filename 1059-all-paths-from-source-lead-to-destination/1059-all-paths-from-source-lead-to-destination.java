a104be365e62bffef98f61ab467251a1d98898dd
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-02 18:26:30 +0530

    Time: 16 ms (49.77%), Space: 54.8 MB (62.68%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.java
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.java
@@ -1,8 +1,11 @@
 class Solution {
+    
+    // 1) DFS 1: Mark color first and then call dfs
     public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
         List<Integer>[] adjList = getAdjList(n, edges);
         
         int[] color = new int[n];
+        color[source] = 1;
         return leadsToDestinationDfs(adjList, source, destination, color);
     }
     
@@ -11,18 +14,46 @@ class Solution {
         if(nbrs.isEmpty())
             return src==dest;
         
-        color[src] = 1;
         for(int nbr : nbrs) {
             if(color[nbr]==0) {
+                color[nbr] = 1;
+                
                 if(!leadsToDestinationDfs(adjList, nbr, dest, color))
                     return false;
-            } else if(color[nbr]==1) 
+                
+                color[nbr] = 2;
+            } else if(color[nbr]==1)
                 return false;
         }
-        
-        color[src] = 2;
         return true;
     }
+    
+    
+    // 2) DFS 2: Call dfs first and then mark node
+//     public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
+//         List<Integer>[] adjList = getAdjList(n, edges);
+        
+//         int[] color = new int[n];
+//         return leadsToDestinationDfs(adjList, source, destination, color);
+//     }
+    
+//     private boolean leadsToDestinationDfs(List<Integer>[] adjList, int src, int dest, int[] color) {
+//         List<Integer> nbrs = adjList[src];
+//         if(nbrs.isEmpty())
+//             return src==dest;
+        
+//         color[src] = 1;
+//         for(int nbr : nbrs) {
+//             if(color[nbr]==0) {
+//                 if(!leadsToDestinationDfs(adjList, nbr, dest, color))
+//                     return false;
+//             } else if(color[nbr]==1) 
+//                 return false;
+//         }
+        
+//         color[src] = 2;
+//         return true;
+//     }
     
     private List<Integer>[] getAdjList(int n, int[][] edges) {
         List<Integer>[] adjList = new ArrayList[n];
