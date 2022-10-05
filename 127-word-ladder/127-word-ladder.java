1ebeabbd9d7b8085fe99cffe20958498c5ac33aa
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-06 03:58:36 +0530

    Time: 204 ms (42.72%), Space: 86.8 MB (44.33%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,41 +1,96 @@
 class Solution {
+    
+    // 1) BFS
+//     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//         if(beginWord.equals(endWord))
+//             return 0;
+        
+//         Set<String> words = new HashSet<>(wordList);
+//         if(!words.contains(endWord))
+//             return 0;
+        
+//         Queue<String> q = new ArrayDeque<>();
+//         q.add(beginWord);
+        
+//         int seq = 1;
+//         while(!q.isEmpty()) {
+//             int size = q.size();
+            
+//             seq++;
+//             for(int i=0 ; i<size ; i++) {
+//                 char[] word = q.remove().toCharArray();
+                
+//                 for(int j=0 ; j<word.length ; j++) {
+//                     for(char k='a' ; k<='z' ; k++) {
+//                         char actual = word[j];
+//                         word[j] = k;
+                        
+//                         String newWord = new String(word);
+//                         if(newWord.equals(endWord))
+//                             return seq;
+//                         if(words.contains(newWord)) {
+//                             q.add(newWord);
+//                             words.remove(newWord);
+//                         }
+                        
+//                         word[j] = actual;
+//                     }
+//                 }
+//             }
+//         }
+//         return 0;
+//     }
+    
+    
+    // 2) Bidirectional BFS
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(beginWord.equals(endWord))
             return 0;
-        
         Set<String> words = new HashSet<>(wordList);
         if(!words.contains(endWord))
             return 0;
         
-        Queue<String> q = new ArrayDeque<>();
-        q.add(beginWord);
+        Set<String> beginSet = new HashSet<>();
+        beginSet.add(beginWord);
+        Set<String> endSet = new HashSet<>();
+        endSet.add(endWord);
         
         int seq = 1;
-        while(!q.isEmpty()) {
-            int size = q.size();
-            
+        while(!beginSet.isEmpty() && !endSet.isEmpty()) {
             seq++;
-            for(int i=0 ; i<size ; i++) {
-                char[] word = q.remove().toCharArray();
+            if(beginSet.size() > endSet.size()) {
+                swap(beginSet, endSet);
+            }
+            
+            Set<String> newSet = new HashSet<>();
+            for(String word : beginSet) {
+                char[] wordArr = word.toCharArray();
                 
-                for(int j=0 ; j<word.length ; j++) {
-                    for(int k=0 ; k<26 ; k++) {
-                        char actual = word[j];
-                        word[j] = (char)('a'+k);
+                for(int i=0 ; i<wordArr.length ; i++) {
+                    char orig = wordArr[i];
+                    for(char j='a' ; j<='z' ; j++) {
+                        wordArr[i] = j;
                         
-                        String newWord = new String(word);
-                        if(newWord.equals(endWord))
+                        String newWord = new String(wordArr);
+                        if(endSet.contains(newWord))
                             return seq;
                         if(words.contains(newWord)) {
-                            q.add(newWord);
+                            newSet.add(newWord);
                             words.remove(newWord);
                         }
                         
-                        word[j] = actual;
+                        wordArr[i] = orig;
                     }
                 }
             }
+            beginSet = newSet;
         }
         return 0;
+    }
+    
+    private void swap(Set<String> begin, Set<String> end) {
+        Set<String> temp = begin;
+        begin = end;
+        end = temp;
     }
 }
