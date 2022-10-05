ade367442f9f9ab8165ae8fbf19c5b0fd2ec0478
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-05 15:48:51 +0530

    Time: 26 ms (35.87%), Space: 53.7 MB (81.47%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -0,0 +1,53 @@
+class Solution {
+    
+    public int minimumSemesters(int n, int[][] relations) {
+        List<Integer>[] adjList = new ArrayList[n+1];
+        int[] indegree = new int[n+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        // Create adjacency list
+        // for(int i=1 ; i<=n ; i++)
+        //     adjList[i] = new ArrayList<>();
+        
+        for(int[] relation : relations) {
+            int src = relation[0];
+            int dest = relation[1];
+            
+            if(adjList[src]==null)
+                adjList[src] = new ArrayList<>();
+            if(adjList[dest]==null)
+                adjList[dest] = new ArrayList<>();
+            
+            adjList[src].add(dest);
+            indegree[dest]++;
+        }
+        
+        // Populate Queue
+        for(int i=1 ; i<=n ; i++) {
+            if(indegree[i]==0)
+                q.add(i);
+        }
+        
+        // Perform topological sort using BFS
+        int sems = 0;
+        int courses = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            sems++;
+            for(int i=0 ; i<size ; i++) {
+                int course = q.remove();
+                courses++;
+                
+                if(adjList[course]!=null) {
+                    for(int nbr : adjList[course]) {
+                        indegree[nbr]--;
+                        if(indegree[nbr]==0)
+                            q.add(nbr);
+                    }
+                }
+            }
+        }
+        return courses==n ? sems : -1;
+    }
+}
