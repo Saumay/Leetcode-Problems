77da3973b4f875ff8f2272dbf8c248cae340babd
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-07 17:27:44 +0530

    Time: 66 ms (18.07%), Space: 64.6 MB (58.01%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -29,71 +29,75 @@ class Solution {
     
     
     // 2) Using DFS + weight sorted edges + distance array
+//     public int networkDelayTime(int[][] times, int n, int k) {
+//         int[] dist = new int[n+1];
+//         List<int[]>[] adjList = getAdjAndDistList(times, n, dist);
+        
+//         // Sorting edges to enable greedy approach
+//         for(int i=1 ; i<=n ; i++) {
+//             Collections.sort(adjList[i], (i1,i2) -> i1[1]-i2[1]);
+//         }
+        
+//         dist[k] = 0;
+//         dfs(adjList, k, 0, dist);
+        
+//         int maxTime = Integer.MIN_VALUE;
+//         for(int i=1 ; i<=n ; i++)
+//             maxTime = Math.max(maxTime, dist[i]);
+        
+//         return maxTime==Integer.MAX_VALUE ? -1 : maxTime;
+//     }
+    
+//     private void dfs(List<int[]>[] adjList, int src, int time, int[] dist) {
+//         List<int[]> nbrs = adjList[src];
+//         for(int[] nbr : nbrs) {
+//             int nbrId = nbr[0];
+//             int nbrTime = time+nbr[1];
+//             if(nbrTime < dist[nbrId]) {
+//                 dist[nbrId] = nbrTime;
+//                 dfs(adjList, nbrId, nbrTime, dist);
+//             }
+//         }
+//     }
+    
+    
+    // 3) Using BFS + Distance Array
     public int networkDelayTime(int[][] times, int n, int k) {
-        int[] dist = new int[n+1];
+        int[] dist = new int[n+1];        
         List<int[]>[] adjList = getAdjAndDistList(times, n, dist);
         
-        // Sorting edges to enable greedy approach
         for(int i=1 ; i<=n ; i++) {
             Collections.sort(adjList[i], (i1,i2) -> i1[1]-i2[1]);
         }
         
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {k,0});
         dist[k] = 0;
-        dfs(adjList, k, 0, dist);
         
-        int maxTime = Integer.MIN_VALUE;
-        for(int i=1 ; i<=n ; i++)
-            maxTime = Math.max(maxTime, dist[i]);
-        
-        return maxTime==Integer.MAX_VALUE ? -1 : maxTime;
-    }
-    
-    private void dfs(List<int[]>[] adjList, int src, int time, int[] dist) {
-        List<int[]> nbrs = adjList[src];
-        for(int[] nbr : nbrs) {
-            int nbrId = nbr[0];
-            int nbrTime = time+nbr[1];
-            if(nbrTime < dist[nbrId]) {
-                dist[nbrId] = nbrTime;
-                dfs(adjList, nbrId, nbrTime, dist);
+        while(!q.isEmpty()) {
+            int[] cur = q.remove();
+            
+            List<int[]> nbrs = adjList[cur[0]];
+            for(int[] nbr : nbrs) {
+                if(dist[cur[0]] + nbr[1] < dist[nbr[0]]) {
+                    dist[nbr[0]] = dist[cur[0]] + nbr[1];
+                    q.add(new int[] {nbr[0], dist[nbr[0]]});
+                }
             }
         }
+        
+        Integer max = Integer.MIN_VALUE;
+        for(int i=1 ; i<=n ; i++) {
+            max = Math.max(max, dist[i]);
+        }
+        return max==Integer.MAX_VALUE ? -1 : max;
     }
-    
-    
-    // 1) Djikstra Algorithm: Using Queue
-//     public int networkDelayTime(int[][] times, int n, int k) {
-//         int[] dist = new int[n+1];        
-//         List<int[]>[] adjList = getAdjList(times, n, dist);
-        
-//         Queue<int[]> q = new ArrayDeque<>();
-//         q.add(new int[] {k,0});
-//         dist[k] = 0;
-        
-//         while(!q.isEmpty()) {
-//             int[] cur = q.remove();
-            
-//             List<int[]> nbrs = adjList[cur[0]];
-//             for(int[] nbr : nbrs) {
-//                 if(dist[cur[0]] + nbr[1] < dist[nbr[0]]) {
-//                     dist[nbr[0]] = dist[cur[0]] + nbr[1];
-//                     q.add(new int[] {nbr[0], dist[nbr[0]]});
-//                 }
-//             }
-//         }
-        
-//         Integer max = Integer.MIN_VALUE;
-//         for(int i=1 ; i<=n ; i++) {
-//             max = Math.max(max, dist[i]);
-//         }
-//         return max==Integer.MAX_VALUE ? -1 : max;
-//     }
     
     
     // 2) Djikstra Algorithm: Using Priority Queue
 //     public int networkDelayTime(int[][] times, int n, int k) {
 //         int[] dist = new int[n+1];
-//         List<int[]>[] adjList = getAdjList(times, n, dist);
+//         List<int[]>[] adjList = getAdjAndDistList(times, n, dist);
         
 //         PriorityQueue<int[]> pq = new PriorityQueue<>((i1,i2) -> i1[1]-i2[1]);
 //         pq.add(new int[] {k,0});
