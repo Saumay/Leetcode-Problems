65e19bec1f0680198d7b498a3509969a78aeb011
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-07 19:37:09 +0530

    Time: 156 ms (27.01%), Space: 61.6 MB (39.13%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -41,7 +41,7 @@ class Solution {
 //     }
     
     
-    // 2) Djikstra's Algorithm
+    // 2.1) Djikstra's Algorithm without early exit
     private final int[][] dirs = new int[][] {{1,0},{0,1},{-1,0},{0,-1}};
     
     public int minimumEffortPath(int[][] heights) {
@@ -59,10 +59,6 @@ class Solution {
             int curI = cur[0];
             int curJ = cur[1];
             
-            // Cirtical: If destination is polled from the queue, we know that there can't be any better path to reach than what we currently have
-            if(curI==m-1 && curJ==n-1)
-                return cur[2];
-            
             for(int[] dir : dirs) {
                 int nbrI = cur[0] + dir[0];
                 int nbrJ = cur[1] + dir[1];
@@ -76,6 +72,45 @@ class Solution {
                 }
             }
         }
-        return 0;
+        return dist[m-1][n-1];
     }
+    
+    
+    // 2) Djikstra's Algorithm
+//     private final int[][] dirs = new int[][] {{1,0},{0,1},{-1,0},{0,-1}};
+    
+//     public int minimumEffortPath(int[][] heights) {
+//         int m = heights.length;
+//         int n = heights[0].length;
+        
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((i1,i2) -> i1[2]-i2[2]);
+//         pq.add(new int[] {0,0,0});
+        
+//         Integer[][] dist = new Integer[m][n];
+//         dist[0][0] = 0;
+        
+//         while(!pq.isEmpty()) {
+//             int[] cur = pq.poll();
+//             int curI = cur[0];
+//             int curJ = cur[1];
+            
+//             // Cirtical: If destination is polled from the queue, we know that there can't be any better path to reach than what we currently have
+//             if(curI==m-1 && curJ==n-1)
+//                 return cur[2];
+            
+//             for(int[] dir : dirs) {
+//                 int nbrI = cur[0] + dir[0];
+//                 int nbrJ = cur[1] + dir[1];
+                
+//                 if(nbrI>=0 && nbrJ>=0 && nbrI<m && nbrJ<n) {
+//                     int nbrDist = Math.max(cur[2], Math.abs(heights[curI][curJ] - heights[nbrI][nbrJ]));
+//                     if(dist[nbrI][nbrJ]==null || (nbrDist<dist[nbrI][nbrJ])) {
+//                         dist[nbrI][nbrJ] = nbrDist;
+//                         pq.add(new int[] {nbrI, nbrJ, nbrDist});
+//                     }
+//                 }
+//             }
+//         }
+//         return 0;
+//     }
 }
