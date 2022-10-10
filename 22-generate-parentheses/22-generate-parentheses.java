d730d2438abadfe508e08be2cc3d485a76eb5b83
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-10 12:38:25 +0530

    Time: 1 ms (98.00%), Space: 43.6 MB (56.22%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,30 +1,30 @@
 class Solution {
     
     // 1) Backtracking
-//     public List<String> generateParenthesis(int n) {
-//         List<String> result = new LinkedList<>();
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new LinkedList<>();
         
-//         backtrack(0, n, n, new StringBuilder(), result);
-//         return result;
-//     }
+        backtrack(0, n, n, new StringBuilder(), result);
+        return result;
+    }
     
-//     private void backtrack(int idx, int open, int close, StringBuilder sb, List<String> result) {
-//         if(open==0 && close==0) {
-//             result.add(sb.toString());
-//             return;
-//         }
+    private void backtrack(int idx, int open, int close, StringBuilder sb, List<String> result) {
+        if(open==0 && close==0) {
+            result.add(sb.toString());
+            return;
+        }
         
-//         if(open>=0) {
-//             sb.append('(');
-//             backtrack(idx+1, open-1, close, sb, result);
-//             sb.deleteCharAt(sb.length()-1);
-//         }
-//         if(close>open) {
-//             sb.append(')');
-//             backtrack(idx+1, open, close-1, sb, result);
-//             sb.deleteCharAt(sb.length()-1);
-//         }
-//     }
+        if(open>0) {
+            sb.append('(');
+            backtrack(idx+1, open-1, close, sb, result);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close>open) {
+            sb.append(')');
+            backtrack(idx+1, open, close-1, sb, result);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
     
     
     // 2) Recursion
@@ -51,32 +51,32 @@ class Solution {
     
     
     // 3) Recursion + Memoization
-    public List<String> generateParenthesis(int n) {
-        return generateParenthesis(n, new HashMap<>());
-    }
+//     public List<String> generateParenthesis(int n) {
+//         return generateParenthesis(n, new HashMap<>());
+//     }
     
-    public List<String> generateParenthesis(int n, Map<Integer, List<String>> cache) {
-        if(cache.containsKey(n)) {
-            return cache.get(n);
-        }
+//     public List<String> generateParenthesis(int n, Map<Integer, List<String>> cache) {
+//         if(cache.containsKey(n)) {
+//             return cache.get(n);
+//         }
 
-        List<String> cur = new LinkedList<>();
-        if(n==0) {
-            cur.add("");
-            return cur;
-        }
+//         List<String> cur = new LinkedList<>();
+//         if(n==0) {
+//             cur.add("");
+//             return cur;
+//         }
         
-        for(int i=0 ; i<n ; i++) {
-            List<String> leftPart = generateParenthesis(i, cache);
-            List<String> rightPart = generateParenthesis(n-i-1, cache);
+//         for(int i=0 ; i<n ; i++) {
+//             List<String> leftPart = generateParenthesis(i, cache);
+//             List<String> rightPart = generateParenthesis(n-i-1, cache);
             
-            for(String left : leftPart) {
-                for(String right : rightPart) {
-                    cur.add('(' + left + ')' + right);
-                }
-            }
-        }
-        cache.put(n, cur);
-        return cur;
-    }
+//             for(String left : leftPart) {
+//                 for(String right : rightPart) {
+//                     cur.add('(' + left + ')' + right);
+//                 }
+//             }
+//         }
+//         cache.put(n, cur);
+//         return cur;
+//     }
 }
