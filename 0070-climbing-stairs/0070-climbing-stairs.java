40bbb400b1af51eb632e705df393c440d0d44310
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-14 16:44:33 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (85.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,20 +8,34 @@ class Solution {
 //         return climbStairs(n-1) + climbStairs(n-2);
 //     }
     
+    
     // 2) Recursion + Memoization
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+        
+//         return climbStairs(n, dp);
+//     }
+    
+//     private int climbStairs(int n, int[] dp) {
+//         if(n<2)
+//             return 1;
+//         if(dp[n] != 0)
+//             return dp[n];
+        
+//         dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
+//         return dp[n];
+//     }
+    
+    
+    // 3) Tabulation
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         
-        return climbStairs(n, dp);
-    }
-    
-    private int climbStairs(int n, int[] dp) {
-        if(n<2)
-            return 1;
-        if(dp[n] != 0)
-            return dp[n];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2 ; i<=n ; i++)
+            dp[i] = dp[i-1] + dp[i-2];
         
-        dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
         return dp[n];
     }
 }
