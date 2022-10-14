e869c31f47cb290094e24458b3c09ac396ae9325
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-14 20:04:50 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (63.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -10,21 +10,23 @@ class Solution {
     
     
     // 2) Recursion + Memoization
-//     public int climbStairs(int n) {
-//         int[] dp = new int[n+1];
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
         
-//         return climbStairs(n, dp);
-//     }
+        return climbStairs(n, dp);
+    }
     
-//     private int climbStairs(int n, int[] dp) {
-//         if(n<2)
-//             return 1;
-//         if(dp[n] != 0)
-//             return dp[n];
+    private int climbStairs(int n, int[] dp) {
+        if(n==0)
+            return 1;
+        if(n<0)
+            return 0;
+        if(dp[n]>0)
+            return dp[n];
         
-//         dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
-//         return dp[n];
-//     }
+        dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
+        return dp[n];
+    }
     
     
     // 3) Tabulation
@@ -41,16 +43,16 @@ class Solution {
     
     
     // 4) Tabulation + Space Optimization
-    public int climbStairs(int n) {        
-        int first = 1;
-        int second = 1;
-        for(int i=2 ; i<=n ; i++) {
-            int cur = first + second;
+//     public int climbStairs(int n) {        
+//         int first = 1;
+//         int second = 1;
+//         for(int i=2 ; i<=n ; i++) {
+//             int cur = first + second;
             
-            first = second;
-            second = cur;
-        }
+//             first = second;
+//             second = cur;
+//         }
         
-        return second;
-    }
+//         return second;
+//     }
 }
