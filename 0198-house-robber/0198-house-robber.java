468879695e4fa6e8d1337dc6a728930eac81507f
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2022-10-16 15:00:32 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (35.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,32 @@
 class Solution {
+    // 1) Tabulation
     public int rob(int[] nums) {
         int n = nums.length;
         
         int[] dp = new int[n];
-        dp[0] = nums[0];
         
-        for(int i=1 ; i<n ; i++) {
-            dp[i] = dp[i-1];
-            
-            if(i-2 >= 0)
-                dp[i] = Math.max(dp[i], dp[i-2] + nums[i]);
-            else
-                dp[i] = Math.max(dp[i], nums[i]);
+        for(int i=0 ; i<n ; i++) {
+            dp[i] = Math.max(getDefaultVal(dp, i-1), getDefaultVal(dp, i-2) + nums[i]);
         }
         return dp[n-1];
     }
+    
+    private int getDefaultVal(int[] dp, int idx) {
+        return idx<0 ? 0 : dp[idx];
+    }
+    
+    
+    // 2) Tabulation with space optimization
+//     public int rob(int[] nums) {
+//         int prevSum = 0;
+//         int curSum = 0;
+        
+//         for(int num : nums) {
+//             int temp = curSum;
+            
+//             curSum = Math.max(curSum, prevSum + num);
+//             prevSum = temp;
+//         }
+//         return curSum;
+//     }
 }
