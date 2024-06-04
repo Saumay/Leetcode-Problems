dd8176e9965b143cef0a12b68b42e4456bb938c3
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2024-06-04 22:48:03 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (42.91%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s.isEmpty())
+            return 0;
+        
+        int idx = 0;
+        int sign = 1;
+        int result = 0;
+        char firstCh = s.charAt(idx);
+        
+        // Check for sign and first Index
+        if(firstCh == '-') {
+            sign = -1; 
+        } else if(firstCh == '+') {
+            sign = 1; 
+        } else if(Character.isDigit(firstCh)) {
+            result = firstCh-'0';
+        } else {
+            return 0;
+        }
+        
+        for(idx=1 ; idx<s.length() ; idx++) {
+            char ch = s.charAt(idx);
+            int digit = ch - '0';
+            if(!Character.isDigit(ch))
+                return sign*result;
+            if(result>Integer.MAX_VALUE/10 || (result==Integer.MAX_VALUE/10 && digit>Integer.MAX_VALUE%10))
+                return sign==-1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            
+            result = result*10 + digit;
+        }
+        return sign*result;
+    }
+    
+}
