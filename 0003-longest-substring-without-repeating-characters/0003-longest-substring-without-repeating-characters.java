a5d0dd3d17119952e20460994bfddcd0564b43e7
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2024-06-04 19:13:04 +0530

    Time: 5 ms (86.56%), Space: 44.4 MB (60.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,15 +1,34 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        Set<Character> seen = new HashSet<>();
+    
+    // Solution 1: Sliding window
+//     public int lengthOfLongestSubstring(String s) {
+//         Set<Character> seen = new HashSet<>();
+//         int left = 0;
+//         int ans = 0;
+        
+//         for(int right=0 ; right<s.length() ; right++) {
+//             while(seen.contains(s.charAt(right))) {
+//                 seen.remove(s.charAt(left));
+//                 left++;
+//             }
+//             seen.add(s.charAt(right));
+//             ans = Math.max(ans, right-left+1);
+//         }
+//         return ans;
+//     }
+    
+    // Solution 2: Using map to store last occurence of a character
+        public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> seen = new HashMap<>();
         int left = 0;
         int ans = 0;
         
         for(int right=0 ; right<s.length() ; right++) {
-            while(seen.contains(s.charAt(right))) {
-                seen.remove(s.charAt(left));
-                left++;
-            }
-            seen.add(s.charAt(right));
+            char rightCh = s.charAt(right);
+            if(seen.containsKey(rightCh) && left<=seen.get(rightCh))
+                left = seen.get(rightCh)+1;
+            
+            seen.put(rightCh, right);
             ans = Math.max(ans, right-left+1);
         }
         return ans;
